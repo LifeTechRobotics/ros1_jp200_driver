9caad75c4f574dddacf5ee41b974473ad6c898e7
motii <108280115+motii8128@users.noreply.github.com>
2024-01-28 17:00:48 +0900

    Add files via upload

--- jp200_driver/src/main.cpp
+++ jp200_driver/src/main.cpp
@@ -7,6 +7,22 @@
 
 using namespace jp200_driver;
 
+class ROS1JP200Driver: public ros::NodeHandle
+{
+    public:
+    ROS1JP200Driver()
+    {
+
+    }
+
+    std::string port_name_;
+    int baud_rate_, servo_num_;
+    bool enable_response_;
+    std::shared_ptr<jp200_driver::JP200Utils> utils;
+    std::vector<jp200_driver::JP200Utils::JP200Cmd> commands;
+
+};
+
 void callback(const jp200_driver::JP200MultiArray::ConstPtr& msg)
 {
 
@@ -23,6 +39,8 @@ int main(int argc, char** argv)
     std::shared_ptr<jp200_driver::JP200Utils> utils;
     std::vector<jp200_driver::JP200Utils::JP200Cmd> commands;
 
+
+    ROS_INFO_STREAM("Set Parameters");
     ros::param::set("port_name", "/dev/ttyUSB0");
     ros::param::set("baud_rate", 1000000);
     ros::param::set("enable_response", true);
@@ -31,12 +49,26 @@ int main(int argc, char** argv)
     ros::param::get("baud_rate", baud_rate_);
     ros::param::get("enable_response", enable_response_);
     ros::param::get("servo_num", servo_num_);
-
     utils = std::shared_ptr<jp200_driver::JP200Utils>(jp200_driver::JP200Utils::getJP200Utils(port_name_, baud_rate_));
 
     for(int i = 0; i < servo_num_; i++)
     {
         auto cmd = JP200Utils::JP200Cmd();
         commands.push_back(cmd);
+    }
+
+    ROS_INFO_STREAM("Init subscriber");
+    ros::Subscriber cmd_subscriber_ = nh.subscribe("jp200_cmd", 1000, callback);
+
+    utils->open_port();
+
+    if(utils->get_fd() < 0)
+    {
+        ROS_ERROR_STREAM("Failed to open port");
+        utils->close_port();
+    }
+    else
+    {
+        ROS_INFO_STREAM("Serial port was connected");
     }
 }
