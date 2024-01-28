9440a87df65fc75623dda923d10676baa3c016c7
motii <108280115+motii8128@users.noreply.github.com>
2024-01-28 16:25:10 +0900

    Add files via upload

--- jp200_driver/src/main.cpp
+++ jp200_driver/src/main.cpp
@@ -1,7 +1,15 @@
 #include <ros/ros.h>
 #include <jp200_driver/JP200MultiArray.h>
 
-int main()
-{
 
+void callback(const jp200_driver::JP200MultiArray::ConstPtr& msg)
+{
+    
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "jp200_driver");
+
+    ros::NodeHandle nh;
 }
